b92523c7b3f601a4684c0a5d9ae583907f4ac573
AB25217 <108901215+AB25217@users.noreply.github.com>
2024-02-09 19:44:01 +0000

    Tested the sieve of erasthomesus and its segmented sieve variant for better memory optimisation
    Although the time complexity was spectacular, the memory usage is incredibly wasteful, and not practical
    
    AB

--- testing_sieve.py
+++ testing_sieve.py
@@ -2,10 +2,40 @@
 Author: Aleksandar Bozov
 File that will complete temporary testing
 """
+import time
+def sieve_of_eratosthenes(limit):
+    primes = []
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(limit**0.5) + 1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(i * i, limit + 1, i):
+                is_prime[j] = False
+
+    for i in range(int(limit**0.5) + 1, limit + 1):
+        if is_prime[i]:
+            primes.append(i)
+
+    return primes
+
+def segmented_sieve(m, n):
+    limit = int(n**0.5)
+    prime_numbers = sieve_of_eratosthenes(limit)
+    numbers = [True] * (n - m + 1)
+
+    for prime in prime_numbers:
+        start = max(prime * prime, (m + prime - 1) // prime * prime)
+        for i in range(start, n + 1, prime):
+            numbers[i - m] = False
+
+    all_prime_numbers = [i for i in range(m, n + 1) if numbers[i - m]]
+
+    return all_prime_numbers
 
 
-
-
-
-
-
+start_time = time.time()
+#print(sieve_of_eratosthenes(10000))
+print(segmented_sieve(15_000_000_000,100_000_000_000))
+print(time.time() - start_time)
